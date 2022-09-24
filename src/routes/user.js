cf580df949d33a41e58c26613c13eeb1c1db5f88
cmanager124 <cmanager124@gmail.com>
2022-09-24 13:24:58 +0530

    all done

--- src/routes/user.js
+++ src/routes/user.js
@@ -12,19 +12,19 @@ router.post('/login', async (req, res) => {
     try {
         const data = await users.findOne({ email: email })
         if (!data) {
-           return res.status(400).json({ message: "USER NOT REGISTERED" })
+            return res.status(400).json({ message: "USER NOT REGISTERED" })
         } else {
             bcrypt.compare(password, data.password, async function (err, result) {
                 if (err) {
-                    return   res.status(500).json({ message: err.message })
+                    return res.status(500).json({ message: err.message })
                 }
                 if (result) {
-                    const token=jwt.sign({
-                        exp: Math.floor(Date.now() / 1000) + (24*60* 60),
+                    const token = jwt.sign({
+                        exp: Math.floor(Date.now() / 1000) + (24 * 60 * 60),
                         data: data._id
                     }, process.env.SECRET);
 
-                  return  res.status(200).json({ message: "Success" ,token})
+                    return res.status(200).json({ message: "Success", token })
                 } else {
                     res.json({ message: "Incorrect Password" })
 
@@ -49,13 +49,13 @@ router.post('/register', body('email').isEmail(), body('password').isLength(min 
         if (!error.isEmpty()) {
             res.status(500).json({ message: error.array() })
         }
-        const data=await users.findOne({email:email})
-        if(data){
+        const data = await users.findOne({ email: email })
+        if (data) {
             return res.status(500).json({
-                message:"email is already registered"
+                message: "email is already registered"
             })
         }
-        
+
         bcrypt.hash(password, 10, async function (err, hash) {
             if (err) {
                 return res.status(400).json({ message: err.message })
